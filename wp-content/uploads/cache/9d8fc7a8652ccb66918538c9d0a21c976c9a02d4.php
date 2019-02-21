fc64fd76327c12f036ecce9eb8f854e62518f5aa
alessandrofuda <alessandro.fuda@gmail.com>
2019-02-21 19:08:59 +0100

    new blade template mounted

--- wp-content/uploads/cache/9d8fc7a8652ccb66918538c9d0a21c976c9a02d4.php
+++ wp-content/uploads/cache/9d8fc7a8652ccb66918538c9d0a21c976c9a02d4.php
@@ -9,7 +9,7 @@
     <div class="wrapper-padded">
       <div id="footer-structure">
         <div class="logo">
-          <img src="<?= App\asset_path('images/wexplore-logo-color.svg'); ?>" alt="Sagde" />
+          <img src="<?= App\asset_path('images/logo-placeholder.png'); ?>" alt="Sagde" />
         </div>
         <div class="hamburger hamburger-footer-typoo">
           <a href="#">Privacy Policy</a> | <a href="#">Cookie Policy</a>
