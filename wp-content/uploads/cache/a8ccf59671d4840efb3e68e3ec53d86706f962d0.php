1f6ba7e56ae41742a769baa0b42cf84959dcd995
alessandrofuda <alessandro.fuda@gmail.com>
2019-02-28 17:35:58 +0100

    navbar

--- wp-content/uploads/cache/a8ccf59671d4840efb3e68e3ec53d86706f962d0.php
+++ wp-content/uploads/cache/a8ccf59671d4840efb3e68e3ec53d86706f962d0.php
@@ -1,21 +1,23 @@
 <header id="header" class="not-scrolled-header">
-<div class="header-bg"></div>
-<div class="wrapper">
-  <div class="wrapper-padded">
-    <div id="header-structure">
-      <div class="logo">
-        <a href="<?php echo e(home_url('/')); ?>"><img src="<?= App\asset_path('images/logo-placeholder.png'); ?>" alt="<?php echo e(get_bloginfo('name', 'display')); ?>" /></a>
-      </div>
-      <div class="hamburger hamburger-typoo">
-      	<nav class="nav-primary">
-	      <?php if(has_nav_menu('primary_navigation')): ?>
-	        <?php echo wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']); ?>
+	<div class="header-bg"></div>
+	<div class="wrapper">
+		<div class="wrapper-padded">
+			<nav class="navbar navbar-expand-lg">
+				<div class="logo">
+					<a class="navbar-brand" href="<?php echo e(home_url('/')); ?>">
+						<img src="<?= App\asset_path('images/logo-placeholder.png'); ?>" alt="<?php echo e(get_bloginfo('name', 'display')); ?>">
+					</a>
+				</div>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<?php if(has_nav_menu('primary_navigation')): ?>
+						<?php echo wp_nav_menu(['theme_location' => 'primary_navigation','menu_class' => 'navbar-nav mr-0 ml-auto']); ?>
 
-	      <?php endif; ?>
-	    </nav>
-        <!--span id="goToSectionOne">Contattaci</span-->
-      </div>
-    </div>
-  </div>
-</div>
+					<?php endif; ?>
+				</div>
+			</nav>
+		</div>
+	</div>
 </header>
