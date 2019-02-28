59876b63bb7b6732a326784612083f26daf02b24
alessandrofuda <alessandro.fuda@gmail.com>
2019-02-28 17:35:58 +0100

    navbar

--- wp-content/themes/sagde/resources/views/partials/header.blade.php
+++ wp-content/themes/sagde/resources/views/partials/header.blade.php
@@ -1,22 +1,22 @@
 <header id="header" class="not-scrolled-header">
-<div class="header-bg"></div>
-<div class="wrapper">
-  <div class="wrapper-padded">
-    <div id="header-structure">
-      <div class="logo">
-        <a href="{{ home_url('/') }}"><img src="@asset('images/logo-placeholder.png')" alt="{{ get_bloginfo('name', 'display') }}" /></a>
-      </div>
-      <div class="hamburger hamburger-typoo">
-      	<nav class="nav-primary">
-          
-	      @if (has_nav_menu('primary_navigation'))
-	        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-	      @endif
-
-	    </nav>
-        <!--span id="goToSectionOne">Contattaci</span-->
-      </div>
-    </div>
-  </div>
-</div>
+	<div class="header-bg"></div>
+	<div class="wrapper">
+		<div class="wrapper-padded">
+			<nav class="navbar navbar-expand-lg">
+				<div class="logo">
+					<a class="navbar-brand" href="{{ home_url('/') }}">
+						<img src="@asset('images/logo-placeholder.png')" alt="{{ get_bloginfo('name', 'display') }}">
+					</a>
+				</div>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					@if (has_nav_menu('primary_navigation'))
+						{!! wp_nav_menu(['theme_location' => 'primary_navigation','menu_class' => 'navbar-nav mr-0 ml-auto']) !!}
+					@endif
+				</div>
+			</nav>
+		</div>
+	</div>
 </header>
